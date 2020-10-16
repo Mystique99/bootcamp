a98ca85a283f65ac513c4ec37d6d91c333c360db
Anabel Franco <anabelfrancoh@gmail.com>
2020-10-15 23:12:13 -0300

    desafio tienda de juegos

---
+++ desafios/m4_vuex/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <p v-text="globalOpen"></p>
+    <p v-text="globalSales"></p>
+    <ul>
+      <li v-for="product in availableProducts" :key="product.id"> 
+        {{product.nombre}}
+      </li>
+    </ul>
+    <ul>
+      <li v-for="product in onSaleProducts" :key="product.id"> 
+        {{product.nombre}}
+      </li>
+    </ul>
+    <h2> Busqueda de Productos Disponibles </h2>
+    <input type="text" v-model="search_id">
+    <ul>
+      <li v-for="product in searchByIdProducts" :key="product.id">
+        {{product.nombre}}
+      </li>
+    </ul> 
+  </div>
+</template>
+
+<script>
+import {mapState, mapGetters} from "vuex"
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      search_id: "",
+      }
+    },
+  computed: {
+    ...mapState(["open", "sales"]),
+    ...mapGetters(["availableProducts", "onSaleProducts", "searchById"]),
+    globalOpen() {
+      let status = this.open
+      ? "abierto": "cerrado"
+      return `estado: ${status}`
+    },
+    globalSales() {
+      return `el total de ventas es: ${this.sales}`
+    },
+    searchByIdProducts() {
+      return this.searchById(this.search_id)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
---
+++ desafios/m4_vuex/src/store/index.js
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        open: false,
+        sales: 0,
+        products: [{
+                id: '0001',
+                nombre: 'Agua sin gas',
+                stock: 30,
+                precio: 670
+            },
+            {
+                id: '0002',
+                nombre: 'Leche extra proteína',
+                stock: 25,
+                precio: 1190
+            },
+            {
+                id: '0003',
+                nombre: 'Huevos Docena',
+                stock: 10,
+                precio: 1590
+            },
+            {
+                id: '0004',
+                nombre: 'Pan marraqueta integral 1kg',
+                stock: 100,
+                precio: 990
+            },
+            {
+                id: '0005',
+                nombre: 'Avena multisemillas',
+                stock: 0,
+                precio: 1390
+            },
+        ]
+    },
+    mutations: {},
+    actions: {},
+    getters: {
+        availableProducts(state) {
+            return state.products.filter(product => product.stock > 0);
+        },
+        onSaleProducts(state) {
+            return state.products.filter(product => product.precio < 1000);
+        },
+        searchById: (_state, getters) => (id) => {
+            return getters.availableProducts.filter(product => product.id == id)
+
+        }
+    }
+})
